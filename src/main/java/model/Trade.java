3460e2a4bb178f4ebe24fc5b34d44cf044003edb
tima <tima@DESKTOP-VTII9VP>
2020-05-15 22:12:14 +0300

    Created Client BaseEntity Changed Trade

--- src/main/java/model/Trade.java
+++ src/main/java/model/Trade.java
@@ -4,85 +4,145 @@ import utils.HttpConnection;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Trade {
-    public String currency1;
-    public String currency2;
-    public String tradeDate;
-    public double amount;
-    public double rate;
-    public double value;
+	protected String currency1;
+	protected String currency2;
+	protected LocalDateTime tradeDate;
+	protected double amount1;
+	protected double rate;
 
-    public Trade(double amount, String currency1, String currency2, double rate, double value, String tradeDate) {
-        this.amount = amount;
-        this.currency1 = currency1;
-        this.currency2 = currency2;
-        this.rate = rate;
-        this.value = value;
-        this.tradeDate = tradeDate;
-    }
+	public Trade(String currency1, String currency2, LocalDateTime tradeDate, double amount1, double rate) {
+		super();
+		this.currency1 = currency1;
+		this.currency2 = currency2;
+		this.tradeDate = tradeDate;
+		this.amount1 = amount1;
+		this.rate = rate;
+	}
 
-    public Trade(){
-    }
+	/**
+	 * @return the currency1
+	 */
+	public String getCurrency1() {
+		return currency1;
+	}
 
-    public void setCurrency1() {
-        Scanner scan = new Scanner(System.in);
-        this.currency1 = scan.nextLine().toUpperCase();
-    }
+	/**
+	 * @param currency1 the currency1 to set
+	 */
+	public void setCurrency1(String currency1) {
+		this.currency1 = currency1;
+	}
 
-    public String getCurrency1() {
-        return currency1;
-    }
+	/**
+	 * @return the currency2
+	 */
+	public String getCurrency2() {
+		return currency2;
+	}
 
-    public void setCurrency2() {
-        Scanner scan = new Scanner(System.in);
-        this.currency2 = scan.nextLine().toUpperCase();
-    }
+	/**
+	 * @param currency2 the currency2 to set
+	 */
+	public void setCurrency2(String currency2) {
+		this.currency2 = currency2;
+	}
 
-    public String getCurrency2() {
-        return currency2;
-    }
+	/**
+	 * @return the tradeDate
+	 */
+	public LocalDateTime getTradeDate() {
+		return tradeDate;
+	}
 
-    public void setAmount() {
-        Scanner scan = new Scanner(System.in);
-        this.amount = Double.parseDouble(scan.nextLine());
-    }
+	/**
+	 * @param tradeDate the tradeDate to set
+	 */
+	public void setTradeDate(LocalDateTime tradeDate) {
+		this.tradeDate = tradeDate;
+	}
 
-    public double getAmount() {
-        return amount;
-    }
+	/**
+	 * @return the amount1
+	 */
+	public double getAmount1() {
+		return amount1;
+	}
 
-    public void setRate(String currency1, String currency2) throws IOException {
-        HttpConnection http = new HttpConnection();
-        this.rate = http.getRate(currency1, currency2);
-    }
+	/**
+	 * @param amount1 the amount1 to set
+	 */
+	public void setAmount1(double amount1) {
+		this.amount1 = amount1;
+	}
 
-    public double getRate() {
-        return rate;
-    }
+	/**
+	 * @return the rate
+	 */
+	public double getRate() {
+		return rate;
+	}
 
-    public void setValue(double rate, double amount) {
-        this.value = rate * amount;
-    }
+	/**
+	 * @param rate the rate to set
+	 */
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
 
-    public double getValue() {
-        return value;
-    }
+	/**
+	 * @return the amount2
+	 */
+	public double getAmount2() {
+		return this.amount1 * this.rate;
+	}
 
-    public void setTradeDate() {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        this.tradeDate = dateFormat.format(date);
-    }
-
-    public String getTradeDate() {
-        return tradeDate;
-    }
-
-    public String toString(){
-        return "Amount: " + amount + ", Currency1: " + currency1 + ", Currency2: " + currency2 +
-                ", Rate: " + rate + ", Value: " + value + ", TradeDate: " + tradeDate;
-    }
+	/*
+	 * public Trade(double amount, String currency1, String currency2, double rate,
+	 * double value, String tradeDate) { this.amount = amount; this.currency1 =
+	 * currency1; this.currency2 = currency2; this.rate = rate; this.value = value;
+	 * this.tradeDate = tradeDate; }
+	 * 
+	 * public Trade(){ }
+	 * 
+	 * public void setCurrency1() { Scanner scan = new Scanner(System.in);
+	 * this.currency1 = scan.nextLine().toUpperCase(); }
+	 * 
+	 * public String getCurrency1() { return currency1; }
+	 * 
+	 * public void setCurrency2() { Scanner scan = new Scanner(System.in);
+	 * this.currency2 = scan.nextLine().toUpperCase(); }
+	 * 
+	 * public String getCurrency2() { return currency2; }
+	 * 
+	 * public void setAmount() { Scanner scan = new Scanner(System.in); this.amount
+	 * = Double.parseDouble(scan.nextLine()); }
+	 * 
+	 * public double getAmount() { return amount; }
+	 * 
+	 * public void setRate(String currency1, String currency2) throws IOException {
+	 * HttpConnection http = new HttpConnection(); this.rate =
+	 * http.getRate(currency1, currency2); }
+	 * 
+	 * public double getRate() { return rate; }
+	 * 
+	 * public void setValue(double rate, double amount) { this.value = rate *
+	 * amount; }
+	 * 
+	 * public double getValue() { return value; }
+	 * 
+	 * public void setTradeDate() { DateFormat dateFormat = new
+	 * SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); Date date = new Date();
+	 * this.tradeDate = dateFormat.format(date); }
+	 * 
+	 * public String getTradeDate() { return tradeDate; }
+	 * 
+	 * public String toString(){ return "Amount: " + amount + ", Currency1: " +
+	 * currency1 + ", Currency2: " + currency2 + ", Rate: " + rate + ", Value: " +
+	 * value + ", TradeDate: " + tradeDate; }
+	 */
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -10,97 +10,139 @@ import java.util.Scanner;
 //listy artyhul https://www.geeksforgeeks.org/arrays-sort-in-java-with-examples/
 //stronka jak odczytac plik https://www.java67.com/2015/08/how-to-load-data-from-csv-file-in-java.html
 
-public class User {
-    public String login;
-    public String name;
-    public String surname;
-    public String emailAddress;
-    public String password;
-    public String creationDate;
-    public int userId;
+public class User extends BaseEntity {
+	protected String login;
+	protected String name;
+	protected String surname;
+	protected String emailAddress;
+	protected String password;
 
-    public User(int userId, String login, String name, String surname, String password, String emailAddress, String creationDate) {
-        this.login = login;
-        this.name = name;
-        this.surname = surname;
-        this.password = password;
-        this.emailAddress = emailAddress;
-        this.creationDate = creationDate;
-        this.userId = userId;
-    }
+	public User(String login, String name, String surname, String emailAddress, String password) {
+		super();
+		this.login = login;
+		this.name = name;
+		this.surname = surname;
+		this.emailAddress = emailAddress;
+		this.password = password;
+	}
 
-    public User() {
-    }
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
 
-    public void setUserLogin () {
-        Scanner scan = new Scanner(System.in);
-        this.login = scan.nextLine();
-    }
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public String getUserLogin(){
-        return login;
-    }
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    public void setUserName () {
-        Scanner scan = new Scanner(System.in);
-        this.name = scan.nextLine();
-    }
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getUserName(){
-        return name;
-    }
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setUserSurname () {
-        Scanner scan = new Scanner(System.in);
-        this.surname = scan.nextLine();
-    }
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public String getUserSurname(){
-        return surname;
-    }
+	/**
+	 * @return the emailAddress
+	 */
+	public String getEmailAddress() {
+		return emailAddress;
+	}
 
-    public void setUserPassword () {
-        Scanner scan = new Scanner(System.in);
-        this.password = scan.nextLine();
-    }
+	/**
+	 * @param emailAddress the emailAddress to set
+	 */
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
 
-    public String getUserPassword(){
-        return password;
-    }
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
 
-    public void setEmailAddress (){
-        Scanner scan = new Scanner(System.in);
-        this.emailAddress = scan.nextLine();
-    }
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getEmailAddress () {
-        return emailAddress;
-    }
-
-    public void setDataCreation (){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        this.creationDate = dateFormat.format(date);
-    }
-
-    public String getCreationDate(){
-        return creationDate;
-    }
-
-    public void setUserId (){
-        Random rand = new Random();
-        int userIdGenerated = rand.nextInt(10000);
-        this.userId = userIdGenerated;
-    }
-
-    public int getUserId (){
-        return userId;
-    }
-
-    public String toString(){
-        return "User ID: " + userId + ", Login: " + login +
-                ", Name: " + name + ", Surname: " + surname +
-                ", Password: " + password + ", Email Address: " + emailAddress +
-                ", Creation Date: " + creationDate;
-    }
+	/*
+	 * public User(int userId, String login, String name, String surname, String
+	 * password, String emailAddress, String creationDate) { this.login = login;
+	 * this.name = name; this.surname = surname; this.password = password;
+	 * this.emailAddress = emailAddress; this.creationDate = creationDate;
+	 * this.userId = userId; }
+	 * 
+	 * public User() { }
+	 * 
+	 * public void setUserLogin () { Scanner scan = new Scanner(System.in);
+	 * this.login = scan.nextLine(); }
+	 * 
+	 * public String getUserLogin(){ return login; }
+	 * 
+	 * public void setUserName () { Scanner scan = new Scanner(System.in); this.name
+	 * = scan.nextLine(); }
+	 * 
+	 * public String getUserName(){ return name; }
+	 * 
+	 * public void setUserSurname () { Scanner scan = new Scanner(System.in);
+	 * this.surname = scan.nextLine(); }
+	 * 
+	 * public String getUserSurname(){ return surname; }
+	 * 
+	 * public void setUserPassword () { Scanner scan = new Scanner(System.in);
+	 * this.password = scan.nextLine(); }
+	 * 
+	 * public String getUserPassword(){ return password; }
+	 * 
+	 * public void setEmailAddress (){ Scanner scan = new Scanner(System.in);
+	 * this.emailAddress = scan.nextLine(); }
+	 * 
+	 * public String getEmailAddress () { return emailAddress; }
+	 * 
+	 * public void setDataCreation (){ DateFormat dateFormat = new
+	 * SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); Date date = new Date();
+	 * this.creationDate = dateFormat.format(date); }
+	 * 
+	 * public String getCreationDate(){ return creationDate; }
+	 * 
+	 * public void setUserId (){ Random rand = new Random(); int userIdGenerated =
+	 * rand.nextInt(10000); this.userId = userIdGenerated; }
+	 * 
+	 * public int getUserId (){ return userId; }
+	 * 
+	 * public String toString(){ return "User ID: " + userId + ", Login: " + login +
+	 * ", Name: " + name + ", Surname: " + surname + ", Password: " + password +
+	 * ", Email Address: " + emailAddress + ", Creation Date: " + creationDate; }
+	 */
 }
